dc2b1d76a50ecf95948cd36d28e5662cbbece1cf
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-25 07:34:43 -0300

    refactor: removing unecessary stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 //router
 import { Router } from './Router';
 
-
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
