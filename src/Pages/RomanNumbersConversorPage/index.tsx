016b71e6d42e8d67fb8913ef67bcf510798d0b07
GTI/WILLYAM.PIMENTEL <10482656417@sdspe.infra>
2022-08-25 16:56:21 -0300

    feat: adding roman number form to the page

--- src/Pages/RomanNumbersConversorPage/index.tsx
+++ src/Pages/RomanNumbersConversorPage/index.tsx
@@ -3,11 +3,13 @@ import './style.css'
 import Container from 'react-bootstrap/Container';
 
 import {NavigationBar} from '../../components/NavigationBar'
+import { RomanNumberForm } from '../../components/RomanNumberForm';
 
 export const RomanNumbersConversorPage = () =>{
     return(
         <Container>
             <NavigationBar />
+            <RomanNumberForm />
         </Container>
     )
 }
