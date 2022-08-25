398f6c94a1bc962e005d784889120a33ab64ed21
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-25 07:57:48 -0300

    adding the pages to the router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,19 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
+//pages
+import { GameOfLifePage } from './pages/GameOfLifePage'
+import { HomePage } from './pages/HomePage'
+import { RestaurantBillDividerPage } from './pages/RestaurantBillDividerPage'
+import { RomanNumbersConversorPage } from './pages/RomanNumbersConversorPage'
+
 export const Router = () =>{
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<h1>hello</h1>} />
-                <Route path='roman-numbers-conversor' element={<h1>roman numbers conversor</h1>} />
-                <Route path='game-of-life' element={<h1>game of life</h1>} />
-                <Route path='restaurant-bill-divider' element={<h1>restaurant bill divider</h1>} />
+                <Route path='game-of-life' element={<GameOfLifePage/>} />    
+                <Route path='/' element={<HomePage/>} />            
+                <Route path='restaurant-bill-divider' element={<RestaurantBillDividerPage/>} />
+                <Route path='roman-numbers-conversor' element={<RomanNumbersConversorPage/>} />
             </Routes>
         </BrowserRouter>
                 
