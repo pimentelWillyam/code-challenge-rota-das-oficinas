fa7722e7221582a4ccbf7f10e32a1c34a27c21fe
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-25 08:39:56 -0300

    feat: adding navbar to the components paste

---
+++ src/components/NavigationBar/index.tsx
@@ -0,0 +1,25 @@
+import './style.css'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export const NavigationBar = () =>{
+    return(
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/home"></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/roman-numbers-conversor">Roman Numbers Conversor</Nav.Link>
+                    <Nav.Link href="/restaurant-bill-divider">Restaurant Bill Divider</Nav.Link>
+                    <Nav.Link href="/game-of-life">Restaurant Bill Divider</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
