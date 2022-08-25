6c296a7c4c9925bcfaeb7c0c43097bb9f8486a79
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-25 07:38:39 -0300

    refactor: changing the " for '

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 export const Router = () =>{
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<h1>hello</h1>} />
-                <Route path="roman-numbers-conversor" element={<h1>roman numbers conversor</h1>} />
-                <Route path="game-of-life" element={<h1>game of life</h1>} />
-                <Route path="restaurant-bill-divider" element={<h1>restaurant bill divider</h1>} />
+                <Route path='/' element={<h1>hello</h1>} />
+                <Route path='roman-numbers-conversor' element={<h1>roman numbers conversor</h1>} />
+                <Route path='game-of-life' element={<h1>game of life</h1>} />
+                <Route path='restaurant-bill-divider' element={<h1>restaurant bill divider</h1>} />
             </Routes>
         </BrowserRouter>
                 
