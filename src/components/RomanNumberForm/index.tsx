deae1db27c607ca5a63d39a6a3d8a7372143f022
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-26 10:25:31 -0300

    feat: finish roman number form component

--- src/components/RomanNumberForm/index.tsx
+++ src/components/RomanNumberForm/index.tsx
@@ -1,15 +1,22 @@
 import './style.css'
 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container'
+import {Form,Button} from 'react-bootstrap';
 
 export const RomanNumberForm = () => {
   return (
-      <Form>
-        <Form.Group className="mb-3" controlId="formRomanNumber">
-          <Form.Control type="text" placeholder="Enter roman number" />
+      <Form id="roman-number-form">
+        <Form.Group className="mb-3">
+          <Form.Label>Disabled select menu</Form.Label>
+          <Form.Select>
+            <option>Insert conversion format</option>
+            <option>Arabic to roman</option>
+            <option>Roman to arabic</option>
+          </Form.Select>
+      </Form.Group>
+        <Form.Group className="mb-3" controlId="formRomanNumberConversion">
+          <Form.Control type="text" placeholder="Insert number to be converted" />
         </Form.Group>
+        <Button id="button-submit-form" variant="primary" type="submit">Convert</Button>
       </Form>
     
   );
