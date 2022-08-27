a67e48fe493a24ab84c9ce5303a681a9dc82deeb
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-27 07:35:14 -0300

    feat: adding handler to roman number form

--- src/handlers/handleRomanNumberFormSubmit.ts
+++ src/handlers/handleRomanNumberFormSubmit.ts
@@ -1,10 +1,29 @@
+import {convertArabicToRoman} from '../helper/convertArabicToRoman'
+import {convertRomanToArabic} from '../helper/convertRomanToArabic'
 
-export const handleRomanNumberFormSubmit = (event: React.FormEvent<HTMLFormElement> ,conversionFormat: string, numberToBeConverted: string | null, setConvertedNumber: React.Dispatch<React.SetStateAction<null>> ) =>{
+export const handleRomanNumberFormSubmit = (event: React.FormEvent<HTMLFormElement> ,conversionFormat: string, numberToBeConverted: string | null, setConvertedNumber: React.Dispatch<React.SetStateAction<string>>, setConversionDone: React.Dispatch<React.SetStateAction<boolean>> ) =>{
    event.preventDefault()
-   if (conversionFormat == 'Arabic to roman'){
-
+   if (conversionFormat == 'Arabic to roman' && numberToBeConverted != null){
+      let conversionResult = convertArabicToRoman(parseInt(numberToBeConverted))
+      if (conversionResult != ''){
+         setConvertedNumber(conversionResult)
+         setConversionDone(true)
+      }
+      else{
+         alert('Invalid format')
+      }
    }
-   else if (conversionFormat == 'Roman to arabic'){
+   else if (conversionFormat == 'Roman to arabic' && numberToBeConverted != null){
+      let conversionResult = convertRomanToArabic(numberToBeConverted).toString()
+      if (conversionResult != ''){
+         setConvertedNumber(conversionResult)
+         setConversionDone(true)
+      }
+      else{
+         alert('Invalid format')
+      }
       
+      
+
    }
 }
