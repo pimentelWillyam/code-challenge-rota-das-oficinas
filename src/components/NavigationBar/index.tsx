5b29ce1be010e8782cd11e5a0c4e5bf93d4ca104
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-27 16:36:31 -0300

    refactor: changing the navbar a lil bit

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,22 +1,23 @@
 import './style.css'
 
+import {Link} from 'react-router-dom'
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const NavigationBar = () =>{
     return(
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="/home"></Navbar.Brand>
+                <Navbar.Brand as = {Link} to="/home"></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/roman-numbers-conversor">Roman Numbers Conversor</Nav.Link>
-                    <Nav.Link href="/restaurant-bill-divider">Restaurant Bill Divider</Nav.Link>
-                    <Nav.Link href="/game-of-life">Game of Life</Nav.Link>
+                    <Nav.Link as = {Link} to="/">Home</Nav.Link>
+                    <Nav.Link as = {Link}  to="/roman-numbers-conversor">Roman Numbers Conversor</Nav.Link>
+                    <Nav.Link as = {Link}  to="/restaurant-bill-divider">Restaurant Bill Divider</Nav.Link>
+                    <Nav.Link as = {Link}  to="/game-of-life">Game of Life</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
