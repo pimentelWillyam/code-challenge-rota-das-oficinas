6eee378f69d9392b8ff70622f301117093c5d22a
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-27 16:37:36 -0300

    refactor: adding some pages to the router

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,8 @@ import { GameOfLifePage } from './pages/GameOfLifePage'
 import { HomePage } from './pages/HomePage'
 import { RestaurantBillDividerPage } from './pages/RestaurantBillDividerPage'
 import { RomanNumbersConversorPage } from './pages/RomanNumbersConversorPage'
+import { AddConsumedProductFormPage } from './pages/AddConsumedProductPage'
+import { AddCostumerPage } from './pages/AddCostumerPage'
 
 export const Router = () =>{
     return(
@@ -13,6 +15,8 @@ export const Router = () =>{
                 <Route path='game-of-life' element={<GameOfLifePage/>} />    
                 <Route path='/' element={<HomePage/>} />            
                 <Route path='restaurant-bill-divider' element={<RestaurantBillDividerPage/>} />
+                <Route path='add-consumed-product' element={<AddConsumedProductFormPage/>} />
+                <Route path='add-costumer' element={<AddCostumerPage/>} />
                 <Route path='roman-numbers-conversor' element={<RomanNumbersConversorPage/>} />
             </Routes>
         </BrowserRouter>
