9d7eb5dbc7701ee1c57624bba78545bc5c8c199f
Willyam Pimentel <pimentelwillyam@gmail.com>
2022-08-28 10:17:09 -0300

    feat: changing the pages a little

--- src/Pages/RestaurantBillDividerPage/index.tsx
+++ src/Pages/RestaurantBillDividerPage/index.tsx
@@ -1,13 +1,25 @@
 import './style.css'
 
-import Container from 'react-bootstrap/Container';
+import {Container,Row} from 'react-bootstrap/';
 
 import {NavigationBar} from '../../components/NavigationBar'
+import {ConsumedProductsTable } from '../../components/Tables/ConsumedProductsTable'
+import {CostumersTable } from '../../components/Tables/CostumersTable'
 
 export const RestaurantBillDividerPage = () =>{
     return(
         <Container>
-            <NavigationBar />
+            <Row id="restaurant-bill-divider-line-1">
+                <NavigationBar />
+            </Row>
+            <Row id="restaurant-bill-divider-line-2">
+                <CostumersTable />
+            </Row>
+            <Row id="restaurant-bill-divider-line-3">
+                <ConsumedProductsTable />
+            </Row>
+
+            
         </Container>
     )
 }
--- src/Pages/RomanNumbersConversorPage/index.tsx
+++ src/Pages/RomanNumbersConversorPage/index.tsx
@@ -3,7 +3,7 @@ import './style.css'
 import {Container,Col,Row} from 'react-bootstrap/'
 
 import {NavigationBar} from '../../components/NavigationBar'
-import { RomanNumberForm } from '../../components/RomanNumberForm';
+import { RomanNumberForm } from '../../components/Forms/RomanNumberForm';
 
 import { useState } from 'react';
 
